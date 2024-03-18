f7d5d9c100e5877db9d04c74d61e94824fd7d383
Dhruv Arora <101944782+itsdhruvarora@users.noreply.github.com>
2024-03-18 07:06:17 +0000

    bajaj done

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.use(bodyParser.json());
+
+
+app.post('/bhfl', async (req, res) => {
+  try {
+   
+    const data = req.body;
+
+    if (!data) {
+      return res.status(400).json({ is_success: false, message: 'No data provided' });
+    }
+
+    const fullName = "Dhruv Arora"; 
+    const dob = "21082003"; 
+    const email = "dhruv2073.be21@chitkara.edu.in"; 
+    const collegeRollNumber = "2110990273"; 
+
+   
+    const userId = `${fullName.toLowerCase().replace(/\s/g, '_')}_${dob}`;
+
+    
+    const evenNumbers = [];
+    const oddNumbers = [];
+    const alphabets = [];
+    for (const item of data) {
+      if (typeof item === 'number') {
+        if (item % 2 === 0) {
+          evenNumbers.push(item);
+        } else {
+          oddNumbers.push(item);
+        }
+      } else if (typeof item === 'string') {
+        alphabets.push(item.toUpperCase());
+      }
+    }
+
+    
+    return res.json({
+      is_success: true,
+      user_id: userId,
+      email: email,
+      college_roll_number: collegeRollNumber,
+      even_numbers: evenNumbers,
+      odd_numbers: oddNumbers,
+      alphabets: alphabets,
+    });
+  } catch (error) {
+    
+    console.error(error);
+    return res.status(500).json({ is_success: false, message: 'An error occurred' });
+  }
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server listening on port ${port}`));
